d4df6cc68ef4399cc950b463b7b45e42de2c0f5e
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2023-07-25 08:37:53 -0300

    added giraffe middleware to server

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,11 +1,26 @@
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Giraffe
+open Microsoft.AspNetCore.Hosting
+
+let webApp =
+    choose [
+        route "/ping"   >=> text "pong"
+    ]
+
 
 [<EntryPoint>]
 let main args =
+
     let builder = WebApplication.CreateBuilder(args)
+    
+    builder.Services.AddGiraffe() |> ignore
+    
+
     let app = builder.Build()
+    
+    app.UseGiraffe (webApp) |> ignore
 
     app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
 
