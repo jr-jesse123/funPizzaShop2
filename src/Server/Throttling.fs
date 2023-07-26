9973e2176b04bbdcd9abe529218dbe687f8d1ffa
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2023-07-26 07:51:50 -0300

    added throttling

---
+++ src/Server/Throttling.fs
@@ -0,0 +1,32 @@
+﻿
+
+module FunPizza.Server.Throttling
+
+open Microsoft.AspNetCore.Http
+open ThrottlingTroll
+
+let setOptions (options: ThrottlingTrollOptions) =
+
+    let idByIp = fun (request:IHttpRequestProxy) -> 
+        let r = (request :?> IIncomingHttpRequestProxy).Request
+        r.HttpContext.Connection.RemoteIpAddress.ToString()
+
+    let PerMinute c = FixedWindowRateLimitMethod(PermitLimit = c, IntervalInSeconds = 60)
+    
+
+    let config = ThrottlingTrollConfig()
+    let login =  
+        ThrottlingTrollRule(
+            UriPattern="/api/Authentication/Login",
+            LimitMethod = PerMinute 5,
+            IdentityIdExtractor = idByIp)
+    let verify = ThrottlingTrollRule(
+            UriPattern="/api/Authentication/Verify",
+            LimitMethod = PerMinute 7,
+            IdentityIdExtractor = idByIp)
+    let verify2 = ThrottlingTrollRule(
+            UriPattern="/api/Authentication/Verify",
+            LimitMethod = FixedWindowRateLimitMethod(PermitLimit = 15, IntervalInSeconds = 600),
+            IdentityIdExtractor = idByIp)
+
+    config.Rules <- [|login; verify; verify2|]
