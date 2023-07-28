d9fda9c609f4ccd1e69adb7ed06126bb615fb6c8
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2023-07-28 15:27:35 -0300

    ADDED DEFAULT HANDLER

---
+++ src/Server/Handlers/Default.fs
@@ -0,0 +1,25 @@
+﻿module FunPizzaShop.Server.Handlers.Default
+open System.Threading.Tasks
+open Giraffe
+open Microsoft.AspNetCore.Http
+open FunPizzaShop.Server.Views
+
+let webApp (env:#_) (layout: HttpContext -> (int -> string Task) -> string Task) = //TODO: criar tipos semanticos para esta assinatura
+    let viewRout view = 
+        fun (next:HttpFunc) (ctx:HttpContext) -> 
+            task {
+                let! lay = view ctx |> layout ctx
+                return! htmlString lay next ctx
+            }
+
+    let defaultRout = viewRout (Index.view env) 
+
+    choose[ routeCi "/" >=> defaultRout]
+
+
+//TODO: UNDERSTAND THE PROPOUSE OF THIS FUNCTION
+let webAppWrapper (env: #_) (layout: HttpContext -> (int -> Task<string>) -> Task<string>) =
+    fun (next:HttpFunc) (ctx:HttpContext) -> 
+        task {
+            return! webApp env layout next ctx
+        }
